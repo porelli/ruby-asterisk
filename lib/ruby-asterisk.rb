62e2b3eb2bec5f23a3555df01aef1f0efb66c070
Michele Porelli <Linux571@gmail.com>
2014-12-16 15:24:27 +0100

    Added timeout on originate and redirect

--- lib/ruby-asterisk.rb
+++ lib/ruby-asterisk.rb
@@ -76,16 +76,16 @@ module RubyAsterisk
       execute 'Status', {'Channel' => channel, 'ActionID' => action_id}
     end
 
-    def originate(caller, context, callee, priority, variable = nil)
-      execute 'Originate', {'Channel' => caller, 'Context' => context, 'Exten' => callee, 'Priority' => priority, 'Callerid' => caller, 'Timeout' => '30000', 'Variable' => variable  }
+    def originate(caller, context, callee, priority, variable = nil, timeout)
+      execute 'Originate', {'Channel' => caller, 'Context' => context, 'Exten' => callee, 'Priority' => priority, 'Callerid' => caller, 'Timeout' => timeout, 'Variable' => variable  }
     end
 
     def channels
       execute 'Command', { 'Command' => 'show channels' }
     end
 
-    def redirect(caller,context,callee,priority,variable=nil)
-      execute 'Redirect', {'Channel' => caller, 'Context' => context, 'Exten' => callee, 'Priority' => priority, 'Callerid' => caller, 'Timeout' => '30000', 'Variable' => variable}
+    def redirect(caller,context,callee,priority,variable=nil, timeout)
+      execute 'Redirect', {'Channel' => caller, 'Context' => context, 'Exten' => callee, 'Priority' => priority, 'Callerid' => caller, 'Timeout' => timeout, 'Variable' => variable}
     end
 
     def queues
